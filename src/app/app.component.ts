4eb0656ee1e116a4cc5bc33a0d8c927d4caef628
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-31 21:10:35 -0300

    Entendendo o método constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'diretivas-angular';
-}
+export class AppComponent implements OnInit{
+  count = 0;
+  
+  constructor(){ }
+
+  ngOnInit(): void { }
+} 
