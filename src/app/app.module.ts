4fbc3813b390f2142a55224c718236dcab1fb421
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-06-03 18:29:14 -0300

    Criando o serviço para acessar a API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { MarcaTextoDirective } from './directives/marca-texto.directive';
@@ -17,7 +18,9 @@ import { ListApiComponent } from './components/list-api/list-api.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [PeopleService],
   bootstrap: [AppComponent]
--- src/app/components/list-api/list-api.component.ts
+++ src/app/components/list-api/list-api.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from 'src/app/shared/services/list.service';
 
 @Component({
   selector: 'app-list-api',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-api.component.css']
 })
 export class ListApiComponent implements OnInit {
+  personagens: Array<any> =[];
 
-  constructor() { }
+  constructor(private listService: ListService) { }
 
   ngOnInit(): void {
+    this.getList();
   }
 
+  getList(){
+    this.listService.getList().subscribe( result => {
+      this.personagens = result;
+      console.log(this.personagens);
+    })
+  }
 }
